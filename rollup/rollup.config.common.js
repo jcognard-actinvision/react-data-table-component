787894533596045732079cca6d7bc16c7eb16e9a
Jérôme Cognard <jcognard@actinvision.com>
2026-04-30 17:02:04 +0200

    fix: make build pass

--- rollup/rollup.config.common.js
+++ rollup/rollup.config.common.js
@@ -1,13 +1,24 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import visualizer from 'rollup-plugin-visualizer';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 
 export const plugins = [
-	resolve({ browser: true, preferBuiltins: true, extensions: ['.ts', '.tsx'] }),
-	commonjs({ include: 'node_modules/**' }),
-	visualizer(),
-	typescript({ check: false, tsconfig: './tsconfig.json' }),
+  resolve({
+    browser: true,
+    preferBuiltins: false,
+    extensions: ['.mjs', '.js', '.jsx', '.json', '.ts', '.tsx'],
+  }),
+  commonjs({
+    include: /node_modules/,
+  }),
+  typescript({
+    tsconfig: './tsconfig.json',
+  }),
+  visualizer(),
 ];
 
-export default { input: './src/index.ts', external: ['react', 'react-dom', 'styled-components'] };
+export default {
+  input: './src/index.ts',
+  external: ['react', 'react-dom', 'styled-components'],
+};
